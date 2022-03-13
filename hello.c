8c01bc74d7ddc4a59d76bd0e1dc7b2f6c17b241f
Cooldrinky <govind.gopal.5384@gmail.com>
2022-03-13 06:54:43 +0000

    Removed input field

--- hello.c
+++ hello.c
@@ -1,8 +1,6 @@
-#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    string name = get_string("What's your name ? ");
-    printf("Hello, %s!\n", name);
+    printf("Hello, world ! \n");
 }
